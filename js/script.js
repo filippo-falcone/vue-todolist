b78e0f9745461130bd05950fc26fccc4869527b8
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-02 13:43:19 +0200

    feat: add tasks array

--- js/script.js
+++ js/script.js
@@ -2,7 +2,32 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return {};
+        return {
+            tasks: [
+                {
+                    text: 'Fare le pulizie',
+                    done: false,
+                },
+                {
+                    text: 'Comprare il latte',
+                    done: true,
+                },
+                {
+                    text: 'Guardare la To Do list',
+                    done: false,
+                },
+                {
+                    text: 'Andare a giocare a calcetto',
+                    done: false,
+                },
+                {
+                    text: 'Scendere in garage',
+                    done: true,
+                }
+            ]
+        };
     },
-    methods: {}
+    methods: {},
+    created() {
+    }
 }).mount('#app');
